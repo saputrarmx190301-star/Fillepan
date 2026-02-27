d9ffd1017b163a294ed572bc8d2696b779866d9c
saputrarmx190301-star <saputrarmx190301@gmail.com>
2026-02-28 03:33:26 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+STORAGE_CHANNEL = os.getenv("STORAGE_CHANNEL")  # JANGAN int()
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
