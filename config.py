78bad54fb230c519931e064e101f7ebde528074d
saputrarmx190301-star <saputrarmx190301@gmail.com>
2026-02-28 02:01:13 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+STORAGE_CHANNEL = int(os.getenv("STORAGE_CHANNEL"))
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+CHANNEL_LINK = "https://t.me/bociku"
