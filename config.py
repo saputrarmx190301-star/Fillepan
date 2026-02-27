c2cedfbc3cdae711437d089c5be7ad6325b031f9
saputrarmx190301-star <saputrarmx190301@gmail.com>
2026-02-28 04:56:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 STORAGE_CHANNEL = os.getenv("STORAGE_CHANNEL")  # JANGAN int()
 CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+GROUP_LINK = "https://t.me/bociku"
+FORCE_CHANNEL = --1003707435475
