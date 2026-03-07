7d04c8fd5136c9a6cac1f331449f061086747772
saputrarmx190301-star <saputrarmx190301@gmail.com>
2026-03-08 00:05:34 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+STORAGE_CHANNEL = int(os.getenv("STORAGE_CHANNEL"))
